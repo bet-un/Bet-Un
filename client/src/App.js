90914ac3bab2a5a8ca02e47422de73783d69a6cb
alexpand <alex.alfara95@gmail.com>
2019-08-22 18:08:08 +0200

    ramaalex

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,17 @@ import Jornada from './components/Jornada'
 import LeagueList from './components/League-list'
 import ClubList from './components/Club-list'
 import NavBar from './components/Nav'
+import Perfil from './components/Perfil'
 import Video from './components/video'
 
 class App extends Component {
 
   constructor() {
     super()
-    this.state = { loggedInUser: null }
+    this.state = {
+      loggedInUser: null,
+      showMenu: false
+    }
     this.authServices = new AuthServices()
   }
 
@@ -32,6 +36,11 @@ class App extends Component {
     }
   }
 
+  handleMenu = () => {
+    const menuState = !this.state.showMenu
+    this.setState({ showMenu: menuState })
+  }
+
   render() {
 
     this.fetchUser()
@@ -41,12 +50,13 @@ class App extends Component {
         <div className="back" >
           <NavBar setUser={this.setTheUser} userInSession={this.state.loggedInUser} />
           {/* <Video /> */}
+
           <Switch>
             <Route path="/LigaSantander" exact component={LeagueList} />
             <Route path="/Clubs" exact component={ClubList} />
             <Route path="/Jornada" exact component={Jornada} />
             <Route path="/Apuesta" exact component={Apuesta} />
-
+            <Route path="/Perfil" exact component={Perfil} />
           </Switch>
         </div>
       );
---
+++ client/src/components/Perfil.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Perfil extends Component {
+
+    constructor() {
+        super()
+
+    }
+
+
+    render() {
+
+        return (
+            <>
+                <h2>Sala privada</h2>
+            </>
+        )
+    }
+}
+export default Perfil
